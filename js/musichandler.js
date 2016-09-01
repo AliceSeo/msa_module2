e0fa81d1bbaf281cddc4766b9194ca176cd0498b
ALICE S <ALICE S>
2016-09-02 02:16:35 +1200

    Add project files and folders

---
+++ js/musichandler.js
@@ -0,0 +1,7 @@
+var myClientId = "46848b5036890e23d2ea5604e06b9568";
+function initSC() {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
---
+++ js/musichandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "46848b5036890e23d2ea5604e06b9568";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
